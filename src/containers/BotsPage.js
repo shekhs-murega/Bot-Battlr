af89bd5c73c9dc90f39e765c931bec34f88bed66
shekinah <nyamburamary1212@gmail.com>
2023-11-03 23:28:32 +0300

    Populating BotsPage

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -3,6 +3,47 @@ import BotCollection from './BotCollection';
 import YourBotArmy from './YourBotArmy';
 import BotSpecs from '../components/BotSpecs';
 
+function BotsPage() {
+    const [botCollection, setBotCollection] = useState([]);
+    const [filteredCollection, setFilteredCollection] = useState([]);
+    const [botArmy, setBotArmy] = useState([]);
+    const [collectionVisible, setCollectionVisible] = useState(true);
+    const [botSpecs, setBotSpecs] = useState({});
+  
+    useEffect(() => {
+      fetch('http://localhost:6001/bots')
+        .then(response => response.json())
+        .then(bots => {
+          setBotCollection(bots);
+          setFilteredCollection(bots);
+        });
+    }, []);
+  
+    const addToArmy = (bot) => {
+      const newCollection = filteredCollection.filter(card => card.bot_class !== bot.bot_class);
+      setFilteredCollection(newCollection);
+      setBotArmy([...botArmy, bot]);
+      setCollectionVisible(true);
+    };
+  
+    const removeFromArmy = (bot) => {
+      const newArmy = botArmy.filter(card => card.id !== bot.id);
+      const armyClasses = newArmy.map(bot => bot.bot_class);
+      const newCollection = botCollection.filter(bot => !armyClasses.includes(bot.bot_class));
+  
+      setBotArmy(newArmy);
+      setFilteredCollection(newCollection);
+    };
+  
+    const removeBotPermanently = (bot) => {
+      const newCollection = botCollection.filter(card => card !== bot);
+      const newFilteredCollection = filteredCollection.filter(card => card !== bot);
+      const newArmy = botArmy.filter(card => card !== bot);
+  
+      setBotCollection(newCollection);
+      setFilteredCollection(newFilteredCollection);
+      setBotArmy(newArmy);
 
+      
 
 export default BotsPage
