6c2df66d0c93e6f4212f7456e268b08083e28166
shekinah <nyamburamary1212@gmail.com>
2023-11-03 23:21:55 +0300

    Populating BotCollection file

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import BotCard from "../components/BotCard";
 
-function BotCollection() {
-  return (
-    <div>BotCollection</div>
-  )
+export default function BotCollection({BotCollection, action, removeCard }) {
+  const displayBotCards = BotCollection.map(bot => {
+    return <BotCard key={bot.id} bot={bot} action={action} removeCard={removeCard} />
+});
+
+    return (
+    <div className='ui for column grid'>
+        <div className='row'>
+           {displayBotCards}
+           You have completed your Bot Army.
+        </div>
+    </div>
+  );
 }
-
-export default BotCollection
