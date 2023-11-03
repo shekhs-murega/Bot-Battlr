db6e82841a78f3d99f3f0a5b188d540fca1a280c
shekinah <nyamburamary1212@gmail.com>
2023-11-03 23:34:52 +0300

    Populating YourBotArmy

--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import BotCard from '../components/BotCard';
 
-export default function YourBotArmy() {
+export default function YourBotArmy(props) {
+  const displayBots = props.bots.map(bot => {
+    return <BotCard key={bot.id} bot={bot} action={props.action} removeCard={props.removeCard} />;
+  });
+
   return (
-    <div>YourBotArmy</div>
-  )
+    <div className="ui segment inverted olive bot-army">
+      <div className="ui five column grid">
+        <div className="row bot-army-row">{displayBots}</div>
+      </div>
+    </div>
+  );
 }
