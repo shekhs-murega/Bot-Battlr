b00dac2b47a48cfd127adb7b50d290e814226b6e
shekinah <nyamburamary1212@gmail.com>
2023-11-03 23:39:29 +0300

    Populating App file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react"
+import BotsPage from "./containers/BotsPage"
+import "./App.css"
 
 export default function App() {
   return (
-    <div>App</div>
+    <div className="App">
+      <BotsPage />
+    </div>
   )
 }
