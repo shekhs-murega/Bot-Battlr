b46f47d3b40be2fdcb823917cecbb6f489692a30
shekinah <nyamburamary1212@gmail.com>
2023-11-03 23:26:57 +0300

    BotsPage file

---
+++ src/containers/BotsPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import BotCollection from './BotCollection';
+import YourBotArmy from './YourBotArmy';
+import BotSpecs from '../components/BotSpecs';
+
+
+
+export default BotsPage
