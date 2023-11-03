ba1c007b2a185402ea5bbbd1fefee850c069450d
shekinah <nyamburamary1212@gmail.com>
2023-11-04 00:08:04 +0300

    Changes in two files

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import BotCard from "../components/BotCard";
+import React from 'react';
+import BotCard from '../components/BotCard';
 
-export default function BotCollection({BotCollection, action, removeCard }) {
-  const displayBotCards = BotCollection.map(bot => {
-    return <BotCard key={bot.id} bot={bot} action={action} removeCard={removeCard} />
-});
+export default function BotCollection({ botCollection, action, removeCard }) {
+  const displayBotCards = botCollection.map(bot => {
+    return <BotCard key={bot.id} bot={bot} action={action} removeCard={removeCard} />;
+  });
 
-    return (
-    <div className='ui for column grid'>
-        <div className='row'>
-           {displayBotCards}
-           You have completed your Bot Army.
-        </div>
+  return (
+    <div className="ui four column grid">
+      <div className="row">
+        {displayBotCards}
+        You have completed your Bot Army.
+      </div>
     </div>
   );
 }
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,75 +1,75 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import BotCollection from './BotCollection';
 import YourBotArmy from './YourBotArmy';
 import BotSpecs from '../components/BotSpecs';
 
 function BotsPage() {
-    const [botCollection, setBotCollection] = useState([]);
-    const [filteredCollection, setFilteredCollection] = useState([]);
-    const [botArmy, setBotArmy] = useState([]);
-    const [collectionVisible, setCollectionVisible] = useState(true);
-    const [botSpecs, setBotSpecs] = useState({});
-  
-    useEffect(() => {
-      fetch('http://localhost:6001/bots')
-        .then(response => response.json())
-        .then(bots => {
-          setBotCollection(bots);
-          setFilteredCollection(bots);
-        });
-    }, []);
-  
-    const addToArmy = (bot) => {
-      const newCollection = filteredCollection.filter(card => card.bot_class !== bot.bot_class);
-      setFilteredCollection(newCollection);
-      setBotArmy([...botArmy, bot]);
-      setCollectionVisible(true);
-    };
-  
-    const removeFromArmy = (bot) => {
-      const newArmy = botArmy.filter(card => card.id !== bot.id);
-      const armyClasses = newArmy.map(bot => bot.bot_class);
-      const newCollection = botCollection.filter(bot => !armyClasses.includes(bot.bot_class));
-  
-      setBotArmy(newArmy);
-      setFilteredCollection(newCollection);
-    };
-  
-    const removeBotPermanently = (bot) => {
-      const newCollection = botCollection.filter(card => card !== bot);
-      const newFilteredCollection = filteredCollection.filter(card => card !== bot);
-      const newArmy = botArmy.filter(card => card !== bot);
-  
-      setBotCollection(newCollection);
-      setFilteredCollection(newFilteredCollection);
-      setBotArmy(newArmy);
+  const [botCollection, setBotCollection] = useState([]);
+  const [filteredCollection, setFilteredCollection] = useState([]);
+  const [botArmy, setBotArmy] = useState([]);
+  const [collectionVisible, setCollectionVisible] = useState(true);
+  const [botSpecs, setBotSpecs] = useState({});
 
-      fetch(`http://localhost:6001/bots/${bot.id}`, {
-        method: 'DELETE'
-      })
-        .then(response => response.json())
-        .then(result => console.log(result));
-    };
-  
-    const displayBotSpecs = (bot) => {
-      setCollectionVisible(false);
-      setBotSpecs(bot);
-    };
-  
-    const displayBotCollection = () => {
-      setCollectionVisible(true);
-    };
-  
-    return (
-      <div>
-        <YourBotArmy bots={botArmy} action={removeFromArmy} removeCard={removeBotPermanently} />
-        {collectionVisible ? (
-          <BotCollection botCollection={filteredCollection} action={displayBotSpecs} removeCard={removeBotPermanently} />
-        ) : (
-          <BotSpecs bot={botSpecs} back={displayBotCollection} enlist={addToArmy} />
-        )}
-      </div>
-    );
-  }
-  
-  export default BotsPage;
+  useEffect(() => {
+    fetch('http://localhost:3000/bots')
+      .then(response => response.json())
+      .then(bots => {
+        setBotCollection(bots);
+        setFilteredCollection(bots);
+      });
+  }, []);
+
+  const addToArmy = (bot) => {
+    const newCollection = filteredCollection.filter(card => card.bot_class !== bot.bot_class);
+    setFilteredCollection(newCollection);
+    setBotArmy([...botArmy, bot]);
+    setCollectionVisible(true);
+  };
+
+  const removeFromArmy = (bot) => {
+    const newArmy = botArmy.filter(card => card.id !== bot.id);
+    const armyClasses = newArmy.map(bot => bot.bot_class);
+    const newCollection = botCollection.filter(bot => !armyClasses.includes(bot.bot_class));
+
+    setBotArmy(newArmy);
+    setFilteredCollection(newCollection);
+  };
+
+  const removeBotPermanently = (bot) => {
+    const newCollection = botCollection.filter(card => card !== bot);
+    const newFilteredCollection = filteredCollection.filter(card => card !== bot);
+    const newArmy = botArmy.filter(card => card !== bot);
+
+    setBotCollection(newCollection);
+    setFilteredCollection(newFilteredCollection);
+    setBotArmy(newArmy);
+
+    fetch(`http://localhost:3000/bots/${bot.id}`, {
+      method: 'DELETE'
+    })
+      .then(response => response.json())
+      .then(result => console.log(result));
+  };
+
+  const displayBotSpecs = (bot) => {
+    setCollectionVisible(false);
+    setBotSpecs(bot);
+  };
+
+  const displayBotCollection = () => {
+    setCollectionVisible(true);
+  };
+
+  return (
+    <div>
+      <YourBotArmy bots={botArmy} action={removeFromArmy} removeCard={removeBotPermanently} />
+      {collectionVisible ? (
+        <BotCollection botCollection={filteredCollection} action={displayBotSpecs} removeCard={removeBotPermanently} />
+      ) : (
+        <BotSpecs bot={botSpecs} back={displayBotCollection} enlist={addToArmy} />
+      )}
+    </div>
+  );
+}
+
+export default BotsPage;
