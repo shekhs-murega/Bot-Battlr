bc14950a43a5f8c63277c2df073fd22c8a5d3d64
shekinah <nyamburamary1212@gmail.com>
2023-11-04 00:34:00 +0300

    Populating BotCollection file

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -10,7 +10,7 @@ export default function BotCollection({ botCollection, action, removeCard }) {
     <div className="ui four column grid">
       <div className="row">
         {displayBotCards}
-        You have completed your Bot Army.
+        You have completed your Bot Army! Thank you.
       </div>
     </div>
   );
